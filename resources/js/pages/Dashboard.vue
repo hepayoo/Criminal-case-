1d30e6c0c2513824556bdde57550d9539c39e11b
hepayo <elgotbihiba2005@gmail.com>
2024-09-08 21:36:25 +0100

    add logout logic

--- resources/js/pages/Dashboard.vue
+++ resources/js/pages/Dashboard.vue
@@ -1,6 +1,7 @@
 <template>
 
     <h1>helllo{{ name }}</h1>
+    <div class="logout"><a href="#" @click="logout">Log out</a></div>
     
 </template>
 
@@ -17,6 +18,15 @@ export default {
       .get("/api/user")
       .then((response) => (this.name = response.data.name))
       .catch((error) => console.log(error));
+  },
+
+  methods: {
+    logout() {
+      axios
+        .post("/api/logout")
+        .then((response) => this.$router.push({ name: "Home" }))
+        .catch((error) => console.log(error));
+    },
   },
 };
 </script>
--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Laravel\Fortify\Http\Controllers\RegisteredUserController;
-
+use Laravel\Fortify\Http\Controllers\AuthenticatedSessionController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -20,4 +20,9 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 //////////////////////////////////////::Public routes::///////////////////////////////////////
+
+//register route//
 Route::post('register', [RegisteredUserController::class, 'store']);
+
+//logout route//
+Route::middleware('auth:sanctum')->post('logout', [AuthenticatedSessionController::class, 'destroy']);
